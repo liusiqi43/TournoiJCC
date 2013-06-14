17ee370dc5ef23932e802547a15944d467b1ef78
siqi <me@siqi.fr>
2013-06-14 21:01:32 +0200

    Match

--- RequêteSQL/funct_nick_lookup.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE OR REPLACE FUNCTION nickname_lookup(IN year int, IN login varchar, OUT nick varchar) AS $$
-    BEGIN
-        SELECT surnom INTO nick
-        FROM tparticipations tp
-        WHERE tp.annee = year AND tp.login = login;
-    END;
-$$ LANGUAGE plpgsql;
